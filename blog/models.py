4a7d8eeff087f4525936f3395b53b5dfce1c7add
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-12-18 21:59:03 +0700

    Moved code to reusable QuerySet method.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Count
+
+
+class PostQuerySet(models.QuerySet):
+
+    def year(self, year):
+        return self.filter(published_at__year=year).order_by('published_at')
+
+    def popular(self):
+        popular_posts = self.annotate(
+            Count('likes')
+        ).order_by('-likes__count').prefetch_related('author')
+        most_popular_posts_ids = [post.id for post in popular_posts]
+        posts_with_comments = Post.objects.filter(
+            id__in=most_popular_posts_ids
+        ).annotate(comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list(
+            'id',
+            'comments_count'
+        )
+        count_for_id = dict(ids_and_comments)
+        for post in popular_posts:
+            post.comments_count = count_for_id[post.id]
+        return popular_posts
+
+
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        return self.annotate(
+            Count('posts')
+        ).order_by('-posts__count')
 
 
 class Post(models.Model):
@@ -9,7 +41,7 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-
+    objects = PostQuerySet.as_manager()
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -39,6 +71,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
